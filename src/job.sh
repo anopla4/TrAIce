303ecb4c2448c01f6cde61f644edca3c8208a24c
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2024-05-24 15:16:56 -0500

    Update bash script

--- src/job.sh
+++ src/job.sh
@@ -1,9 +1,7 @@
-source /data/arguellesa/venv/traice/bin/activate
+#!/bin/bash
+source ../traicenv/bin/activate
 module load python39
-if idlegpu 2>/dev/null 1>&2 ; then
-        export CUDA_VISIBLE_DEVICES=idlegpu -p 1;
-fi;
-# to train
-python model/train.py
-# to test
-# python model/test.py
+# to train uncomment next line
+# python model/train.py
+# to test uncomment next line
+python model/test.py /data/arguellesa/traice/old_weights/checkpoint-500 /data/arguellesa/traice/1.txt 
